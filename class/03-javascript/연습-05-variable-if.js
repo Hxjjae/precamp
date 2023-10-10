48a691459218a5006535f090adf615e2146c7d3a
hxjjae <jhj02201@gmail.com>
2023-10-10 15:50:39 +0900

    수정사항

--- class/03-javascript/연습-05-variable-if.js
+++ class/03-javascript/연습-05-variable-if.js
@@ -55,3 +55,18 @@ if (0) {
 }
 // VM961:4 틀렸습니다
 // undefined
+
+const profile = {
+  name: "철수",
+  age: 12,
+  school: "다람쥐초등학교",
+};
+
+if (profile.age >= 20) {
+  console.log("성인입니다");
+} else if (profile.age >= 8 && profile.age <= 19) {
+  console.log("학생입니다");
+} else {
+  console.log("어린이입니다");
+}
+// VM1896:4 학생입니다
